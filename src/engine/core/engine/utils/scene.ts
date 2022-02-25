fc7ffad93f40d97a5f7f2d3c675e2cff99fe14a9
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2022-02-24 19:10:08 -0600

    Mostly finish scenes

--- src/engine/core/engine/utils/scene.ts
+++ src/engine/core/engine/utils/scene.ts
@@ -15,10 +15,55 @@ export class Scene extends SorseClassCore {
 
 	protected setSprites(sprites: Sprite[]) {
 		this.sceneSprites = sprites;
-		Sorse.emit('render');
+		Sorse.emit("render");
 	}
 
-	constructor() {
+	private parsePercentTemplate(
+		strs: TemplateStringsArray,
+		...extras: unknown[]
+	) {
+		let fullStr = "";
+		// Join strs using extras
+		for (let i = 0; i < strs.length; i++) {
+			fullStr += strs[i];
+			if (i < extras.length) {
+				fullStr += `${extras[i]}`;
+			}
+		}
+
+		const number = parseInt(fullStr.replace(/%/g, ""));
+
+		if (isNaN(number)) {
+			throw new Error(`[Sorse:Scene] Invalid percent string: ${fullStr}`);
+		}
+
+		return number;
+	}
+
+	private getErrorMessageForTemplates(wh: string) {
+		return [
+			`[Sorse:Scene] ${wh} is undefined. Please construct the scene before using templates.`,
+			"If you're using them to set variables, make sure you set them in the constructor or in any methods",
+		].join("\n");
+	}
+
+	protected wP(strs: TemplateStringsArray, ...extras: unknown[]) {
+		if (this.width == undefined)
+			throw new Error(
+				this.getErrorMessageForTemplates("Width")
+			);
+		return (this.width / 100) * this.parsePercentTemplate(strs, ...extras);
+	}
+
+	protected hP(strs: TemplateStringsArray, ...extras: unknown[]) {
+		if (this.height == undefined)
+			throw new Error(
+				this.getErrorMessageForTemplates("Height")
+			);
+		return (this.height / 100) * this.parsePercentTemplate(strs, ...extras);
+	}
+
+	constructor(public readonly width: number, public readonly height: number) {
 		super();
 	}
 }
