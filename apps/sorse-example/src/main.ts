35ef939051baf6fb41be5a6b724faec8f9b0829d
Lukas <69743171+quick007@users.noreply.github.com>
2022-05-05 22:03:27 -0700

    Testing stuff and Easydocs package

--- apps/sorse-example/src/main.ts
+++ apps/sorse-example/src/main.ts
@@ -10,6 +10,8 @@ import {
 	SorseClickType,
 	SorseScene,
 	SorseSprite,
+	Player,
+	
 } from "sorse";
 
 import "./styles.css";
@@ -22,7 +24,7 @@ class MySprite extends SorseSprite {
 		]);
 		this.shapes = [
 			new RoundedRect({
-				color: "purple",
+				color: "red",
 				position: new Position(100, 100),
 				height: 100,
 				width: 100,
@@ -44,6 +46,27 @@ class MySprite extends SorseSprite {
 
 	onClick(pos: Position, type: SorseClickType): void {
 		console.log("You clicked at: ", pos, "With:", type);
+		//this.position = new Position(200, 200);
+		const player = new Player("./sound.mp3")
+		const player2 = new Player("./more-sound.mp3")
+		player.play()
+		setTimeout(() => {
+			player.pause()
+			//player2.play()
+		}, 1000)
+		setTimeout(() => {
+			player.play()
+		}, 1000)
+		// this.shapes.push(
+		// 	new RoundedRect({
+		// 	color: "blue",
+		// 	position: new Position(50, 50),
+		// 	height: 100,
+		// 	width: 100,
+		// 	radius: 10,
+		// }),
+		// )
+		
 	}
 
 	onKeyDown(key: string): void {
@@ -51,9 +74,47 @@ class MySprite extends SorseSprite {
 	}
 }
 
+class AnotherSprite extends SorseSprite {
+	onInit() {
+		this.position = new Position(0, 0);
+		this.collision = new Collision([
+			new Box(new Position(0, 0), new Position(100, 100)),
+		]);
+		this.setState("test", "red");
+		this.shapes = [
+			new RoundedRect({
+				color: this.getState("test")!,
+				position: new Position(0, 0),
+				height: 100,
+				width: 100,
+				radius: 10,
+			}),
+		];
+		setTimeout(() => {
+			this.setState("test", "blue");
+		}, 1000)
+		this.visible = false
+	}
+
+	onClick(pos: Position, type: SorseClickType): void {
+		//this.setState("test", "blue");
+		this.shapes[0] = (new RoundedRect({
+			color: "blue",
+			position: new Position(500, 500),
+			height: 100,
+			width: 100,
+			radius: 10,
+		}))
+	}
+}
+
+
+
 class MyScene extends SorseScene {
-	onInit(): void {
-		this.sprites = [new MySprite()];
+	public onInit(): void {
+		this.sprites = [new MySprite(), new AnotherSprite()];
+		//this.sceneBackground = 
+		
 	}
 
 	//@event("stateChange")
---
+++ packages/easy-docs/src/init.ts
@@ -0,0 +1,11 @@
+/**
+ * Sorse 2
+ *
+ * Developed by Wave-studio
+ */
+
+
+
+export const initSorse = () => {
+	
+};
