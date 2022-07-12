53a898aaf9e13c12f151238293e45390b682625f
Lukas <69743171+quick007@users.noreply.github.com>
2022-07-12 15:00:59 -0700

    push

--- examples/sorse-example/src/main.tsx
+++ examples/sorse-example/src/main.tsx
@@ -1,6 +1,7 @@
-import { Container, Sorse, useState } from "sorse";
+import { Container, Sorse, useState, ShapeReturn } from "sorse";
 import "./style.css";
 import Start from "./pages/start";
+import Error from "./pages/error";
 
 Sorse.init({
 	canvas: {
@@ -9,15 +10,18 @@ Sorse.init({
 			width: window.innerWidth,
 		},
 	},
-	component: () => {
-		const [stage, setStage] = useState(0);
-
-		return (
-			<>
-				<Container visible={true}>
-					<Start />
-				</Container>
-			</>
-		);
-	},
+	component: game
 });
+
+function game(): ShapeReturn {
+	return (
+		<>
+			<Container visible={false}>
+				<Start />
+			</Container>
+			<Container visible={true}>
+				<Error />
+			</Container>
+		</>
+	);
+}
